199d5915e282fbbb3e41daaaecd709f30d12c097
catapoeM <catalin_poenaru95@yahoo.com>
2022-05-03 19:16:10 +0200

    random

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ function startGame() {
   createHoles()
 
   function createHoles() {
-    let parentCanvas = document.getElementById('parentCanvas')
+    const parentCanvas = document.getElementById('parentCanvas')
     let canvas = document.createElement('CANVAS')
     let width = 6, height = 3, left = 10, top = 6;
-    for (let i = 1; i <= 3; ++i) {
+    for (let i = 1, id = 1; i <= 3; ++i) {
       left = 10;
-      for (let j = 1; j <= 3; ++j) {
+      for (let j = 1; j <= 3; ++j, ++id) {
         canvas = document.createElement('CANVAS')
         canvas.style.width = width + 'cm'
         canvas.style.height = height + 'cm'
@@ -16,14 +16,42 @@ function startGame() {
         canvas.style.position = 'absolute'
         canvas.style.left = left + 'cm'
         canvas.style.top = top + 'cm'
+        canvas.setAttribute('id', id)
         canvas.addEventListener('click', function() {
-          alert(i + " " + j)
+          alert(this.getAttribute('id'))
         })
         left += 12
         parentCanvas.appendChild(canvas)
       }
       top += 6
     }
+  }
+
+  addingMole()
+
+  function addingMole(intervalAdding) {
+    clearInterval(intervalAdding)
+    let random = mathRandom()
+    const parentCanvas = document.getElementById('parentCanvas')
+    parentCanvas.childNodes[random].style.backgroundColor = 'yellow'
+    const intervalRemove =  setInterval(function () {
+      removeMole(random, intervalRemove)
+      }, 1000)
+  }
+
+  function removeMole(random, intervalRemove) {
+    clearInterval(intervalRemove)
+    const parentCanvas = document.getElementById('parentCanvas')
+    parentCanvas.childNodes[random].style.backgroundColor = 'white'
+    const intervalAdding = setInterval(function () {
+      addingMole(intervalAdding)
+      }, 1000)
+      
+  }
+
+  function mathRandom() {
+    let random = Math.floor(Math.random() * 9) + 0;
+    return random;
   }
   
   
