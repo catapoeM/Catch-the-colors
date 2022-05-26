fc1a36e8bdeeefa60fcb7d0cd43de4a3b4c45264
catapoeM <catalin_poenaru95@yahoo.com>
2022-05-26 13:58:02 +0200

    init

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function startGame() {
   document.getElementById('startButton').disabled = true;
+
+  // This function remove the main Text which I have at the beginning with the info about the game.
   removeTextInfo()
   function removeTextInfo() {
     const gameInfo = document.querySelectorAll('.gameInfo')
@@ -7,10 +9,11 @@ function startGame() {
       gameInfo[i].innerText = '';
     }
   }
- 
+  // let Variables which I use later in the project
   let points = 0, seconds = 60;
   let speedYellow = 1500, speedGreen = 1450, speedBlue = 1400, speedRed = 1250, youCanAdd = 2;
 
+  // Create 9 canvas (rectAngles), style them and add an Event with a function for each element.
   createRectAngle()
   function createRectAngle() {
     const parentCanvas = document.getElementById('parentCanvas')
@@ -39,10 +42,10 @@ function startGame() {
     }
   }
 
+  // function for the game timer (60 seconds)
   const startTime = setInterval(timeLeft, 1000);
   function timeLeft() {
     --seconds;
-    
     let time = document.getElementById('time')
     time.innerHTML = 'Time: ' + seconds
     if (seconds <= 0) {
@@ -51,6 +54,7 @@ function startGame() {
     }
   }
 
+  // array of numbers where I check the elements, which one has color or not.
   const arrayOfNumbers = [];
   initialiazeArray()
   function initialiazeArray() {
@@ -59,6 +63,7 @@ function startGame() {
     }
   }
   
+  // This function has a parameter. It checks which color is clicked and its property.
   function catchTheColor(id) {
     const parentCanvas = document.getElementById('parentCanvas')
     let elementId = parentCanvas.childNodes[id].getAttribute('clickable')
@@ -71,18 +76,22 @@ function startGame() {
     }else if (elementId == 4) {
       points -= 40;
     }
+    // Remove attribute of some canvas when the color has disappeared
     parentCanvas.childNodes[id].removeAttribute('clickable')
     info()
   }
+
+  // Refresh points information
   info()
   function info() {
     let pointsInfo = document.getElementById('pointsInfo')
     pointsInfo.innerHTML = 'Points: ' + points
   }
 
+  // add first color at some interval
   addYellow()
   function addYellow() {
-    // check if the hole already has a color
+    // check if the rectangle already has a color
     let random;
     for (let i = 0; i < 9; ++i) {
       random = mathRandom.call()
@@ -107,6 +116,7 @@ function startGame() {
     }
   }
 
+  // remove the first color at some interval
   function removeYellow(random) {
     // delete the memory of color
     arrayOfNumbers[random] = 0;
@@ -223,8 +233,8 @@ function startGame() {
     }
   }
 
+  // add color and attribute for a given random rectangle
   function addStyleAndAttribute(random, color) {
-    // Here is the problem "you can add!"
     if (youCanAdd != 0) {
       colors = ['', 'yellow', 'green', 'blue', 'red']
       const parentCanvas = document.getElementById('parentCanvas')
@@ -233,17 +243,20 @@ function startGame() {
     }
   }
 
+  // remove color and attribute for a given random rectangle
   function removeStyleAndAttribute(random) {
     const parentCanvas = document.getElementById('parentCanvas')
     parentCanvas.childNodes[random].style.backgroundColor = 'white'
     parentCanvas.childNodes[random].removeAttribute('clickable')
   }
  
+  // return a random number
   function mathRandom() {
     let random = Math.floor(Math.random() * 10);
     return random;
   }
 
+  // when the game is finished, the reset game button is creating, but the colors stop and no longer are creating
   function gameFinished() {
     const resetButtonParent = document.getElementById('resetButtonParent')
     const resetGameButton = document.createElement('button')
@@ -258,6 +271,7 @@ function startGame() {
     resetButtonParent.appendChild(resetGameButton)
   }
 
+  // clear each rectangle from any color
   function clearTheRectAngles() {
     youCanAdd = 0;
     const parentCanvas = document.getElementById('parentCanvas')
@@ -267,6 +281,8 @@ function startGame() {
     }
   }
 
+  // and reset the game, which remove the reset button, remove every child of the parent canvas,
+  // enables the start button and reinitialized the variables
   function resetGame() {
     const parentCanvas = document.getElementById('parentCanvas')
     const resetButtonParent = document.getElementById('resetButtonParent')
